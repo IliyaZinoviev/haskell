fe66b241c89727ec16effd1c1942897a4f65760c
IliyaZinoviev <mrmar333@yandex.ru>
2019-08-27 22:57:08 +0300

    I solve exercises from chapter 2

---
+++ BinTree.hs
@@ -0,0 +1,17 @@
+module BinTree where
+import Prelude (Int, Char, String, Show(..), (++), Eq(..), Ord(..), Ordering(..))
+import Nat
+
+data BinTree a = Leaf a | BinTree (BinTree a) (BinTree a)
+    deriving (Show)
+
+reverse :: BinTree a -> BinTree a
+reverse (BinTree a b) = (BinTree b a)
+
+depth :: BinTree a -> Nat
+depth (Leaf a) = Zero
+depth (BinTree a b) = Succ (if (compare n1 n2 == GT) 
+                            then n1
+                            else n2)
+    where n1 = depth a
+          n2 = depth b
